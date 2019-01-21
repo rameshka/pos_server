fd54cd791abbd8c6689f43072fed2600638fbb8b
piyumir <rameshkafox@gmail.com>
2019-01-21 17:30:49 +0530

    add JWT auth middleware

---
+++ middleware/middleware.js
@@ -0,0 +1,36 @@
+const jwt = require ('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
+
+// route middleware to verify a token
+module.exports = {
+    protecteroutes:function(req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['authentication'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, secret, function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            }
+            else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+}
+};
