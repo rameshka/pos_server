6fcf7647bf18e9599d253eb71cb4c07323ef3d22
piyumir <rameshkafox@gmail.com>
2019-01-21 17:28:25 +0530

    update route middleware

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cors = require('cors')
+var protectedRouteMiddleware =  require('./middleware/middleware');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
@@ -15,42 +17,12 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/Login',loginRouter);
 
-// route middleware to verify a token
-app.use(function(req, res, next) {
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-  // decode token
-  if (token) {
-
-    // verifies secret and checks exp
-    jwt.verify(token, secret, function(err, decoded) {
-      if (err) {
-      return res.json({ success: false, message: 'Failed to authenticate token.' });
-      }
-      else {
-      // if everything is good, save to request for use in other routes
-      req.decoded = decoded;
-      next();
-    }
-    });
-
-  } else {
-
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-      success: false,
-      message: 'No token provided.'
-    });
-
-  }
-});
-
+app.use(protectedRouteMiddleware.protecteroutes);
 app.use('/order',orderRouter);
 
 
