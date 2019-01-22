5c7f577bf3caddb68c5093e6527d1c79024e2629
piyumir <rameshkafox@gmail.com>
2019-01-23 01:20:25 +0530

    update order schema

--- model/Order.js
+++ model/Order.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const orderItemSchema = new Schema({
+    name: {
+        type: 'String',
+        required: true,
+        trim: true,
+        unique: true
+    },
+    price: {
+        type: 'Decimal128',
+        required: true,
+        trim: true
+    },
+    quantity:{
+        type:'Number',
+        required:'true',
+    }
+});
+
 const orderSchema = new Schema({
     orderID: {
         type: 'String',
@@ -9,11 +27,16 @@ const orderSchema = new Schema({
         unique: true
     },
     cost: {
-        type: 'String',
+        type: 'Decimal128',
         required: true,
         trim: true
     },
+    items:[orderItemSchema],
+    status:{                //open or closed order
+        type:Boolean
+    }
 
 });
 
-module.exports = mongoose.model('User', userSchema); // instance of schema
+module.exports = mongoose.model('Order', orderSchema ); // instance of schema
+module.exports = mongoose.model('OrderItem', orderItemSchema ); // instance of schema
